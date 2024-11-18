be93c1aa5e0479f4b0419a987898bc39c7a72cc2
Muhammad Fathul Amin Sulaiman <fathulamin@ppz.zakat.com.my>
2024-11-18 17:05:01 +0800

    fix

--- src/pages/ehafal/index.tsx
+++ src/pages/ehafal/index.tsx
@@ -2,14 +2,16 @@ import { Toolbar } from "@/features/ehafal/components/table/Toolbar";
 import { EHafalTable } from "@/features/ehafal/components/table/Table";
 import { columns } from "@/features/ehafal/config/columns";
 import { mockData } from "@/features/ehafal/config/mock-data";
+import { useSearch } from "@/features/ehafal/hooks";
 
 export default function EHafal() {
+    const { searchQuery, setSearchQuery, filteredData } = useSearch(mockData);
     return (
         <>
-            <Toolbar />
+            <Toolbar searchQuery={searchQuery} onSearchChange={setSearchQuery} />
             <EHafalTable
                 columns={columns}
-                data={mockData}
+                data={filteredData}
             />
         </>
     );
